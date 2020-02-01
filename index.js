8eed41bfff4bdcb66798711e5f2df0e5bf73e72d
Binhnt <binh.thanhnhu@gmail.com>
2020-02-01 21:04:08 +0700

    ex3

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'pug')
-app.set('views', './views')
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+let users = [
+    { id: 1, name: 'binh' },
+    { id: 2, name: 'anh' }
+]
 
 app.get('/', (req, res) => res.render('index', {
     name: 'aaa'
 }));
-app.get('/users', (req, res) => res.render('users/index', {
-    users: [
-        { name: 'binh' },
-        { name: 'anh' }
-    ]
-}))
+app.get('/users', (req, res) => res.render('users/index', { users }));
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    let matchedUsers = users.filter(user => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render('users/index', { users: matchedUsers });
+});
+
+app.get('/users/create', (req, res) => res.render('users/create'));
+app.post('/users/create', (req, res) => {
+    let user = req.body;
+    users.push(user);
+    res.redirect('/users');
+});
 
 app.listen(port, () => console.log('Server listening on port 3000'))
